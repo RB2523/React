46e9d7e1ea28ec2aefc67fbbe88ce67327f333dd
rajat <rajatengineer25@gmail.com>
2021-08-26 20:19:02 +0530

    react fragment

---
+++ 2 react fragment/1 react.fragment/src/App.js
@@ -0,0 +1,26 @@
+  
+import React from "react";
+  
+
+    //  function App(){
+    //      return (
+    //        <div>
+    //            <h1>First Heading</h1>
+    //            <h1>Second Heading</h1>
+    //        </div>
+    //      );
+    //  }
+
+
+  
+     function App(){
+         return (
+           <React.Fragment>
+               <h1>First Heading</h1>
+               <h1>Second Heading</h1>
+           </React.Fragment>
+         );
+     }
+     
+   
+    export default App;
---
+++ 2 react fragment/2 second syntax/src/App.js
@@ -0,0 +1,26 @@
+  
+// import React , {Fragment} from "react";
+  
+//      function App(){
+//          return (
+//            <Fragment>
+//                <h1>First Heading</h1>
+//                <h1>Second Heading</h1>
+//            </Fragment>
+//          );
+//      }
+
+
+
+  
+     function App(){
+         return (
+           <>
+               <h1>First Heading</h1>
+               <h1>Second Heading</h1>
+           </>
+         );
+     }
+     
+   
+    export default App;
