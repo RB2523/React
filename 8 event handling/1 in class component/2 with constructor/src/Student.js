eafc69213dea1a7c5e9c9d44af3bd60864e648b8
rajat <rajatengineer25@gmail.com>
2021-08-27 12:14:06 +0530

    event handling

--- 8 event handling/1 in class component/2 with constructor/src/Student.js
+++ 8 event handling/1 in class component/2 with constructor/src/Student.js
@@ -1,37 +1,39 @@
    
     import React , {Component} from "react";
 
-      class Student extends Component{
+    class Student extends Component{
 
 
-        constructor(props){
-          super(props);
-          this.handleClick1 = this.handleClick1.bind(this);
-          this.handleClick2 = this.handleClick2.bind(this);
-        }
-
-         handleClick1 = () => {               
-          alert("click occured 1");
-          console.log(this);  
-        }
-
-        handleClick2 = () => {               
-          alert("click occured 2");
-          console.log(this);  
-        }
-
-         render(){
-             return (
-               <div>
-                 <h1>Handling Event</h1>
-                  <button onClick={this.handleClick1}>Click me 1</button>
-                  <button onClick={this.handleClick2}>Click me 2</button>
-              </div>
-          );
-         }
-       
+      constructor(props){
+        super(props);
+        this.handleClick1 = this.handleClick1.bind(this);
+        this.handleClick2 = this.handleClick2.bind(this);
       }
 
-      export default Student;
+        // now in this way you can access this in normal function with click functionality
 
- 
+        handleClick1(){               
+        alert("click occured 1");
+        console.log(this);  
+      }
+
+      handleClick2() {               
+        alert("click occured 2");
+        console.log(this);  
+      }
+
+       render(){
+           return (
+             <div>
+               <h1>Handling Event</h1>
+                <button onClick={this.handleClick1}>Click me 1</button>
+                <button onClick={this.handleClick2}>Click me 2</button>
+            </div>
+        );
+       }
+     
+    }
+
+    export default Student;
+
+
